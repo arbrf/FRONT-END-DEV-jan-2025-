136d1456d0f698ceac284214e60f3d2be8942e64
Ibrahim <shaikmohammadibrahim311@gmail.com>
2025-04-13 10:53:33 +0530

    order review completed

--- ECOMMERCE PROJECT/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ECOMMERCE PROJECT/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import {CartItem} from '../common/cart-item';
-import {Subject} from 'rxjs';
+import {Subject,ReplaySubject,BehaviorSubject} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 cartItems : CartItem[]=[];
-totalPrice:Subject<number>=new Subject<number>();
-totalQuantity:Subject<number>=new Subject<number>();
+totalPrice:Subject<number>=new BehaviorSubject<number>(0);
+totalQuantity:Subject<number>=new BehaviorSubject<number>(0);
 
 
   constructor() { }
