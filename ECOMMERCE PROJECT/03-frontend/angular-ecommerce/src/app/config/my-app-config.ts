3acc9154ed2793fa4d133f93aac6287dba31bd50
Harry <shaikmohammadibrahim311@gmail.com>
2025-04-19 18:29:48 +0530

    Okta added

---
+++ ECOMMERCE PROJECT/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export  default {
+    oidc:{
+        clientId:'0oaocwo4t85yEa2ZZ5d7',
+        issuer:'https://dev-58163707.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes:['openid','profile','email']
+    }
+}
